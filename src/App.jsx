0451a06a83c2b7471df8e24ef325fe5612b34086
adwaidkrishna <adwaidkrishna2@gmail.com>
2026-06-29 16:43:01 +0530

    useState counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
+function App (){
+
+  const [count,setCount]=useState(0)
+
+  function increment(){
+    setCount(count+1)
+  }
+
+  return (
+    <div>
+      <h1>count = {count}</h1>
+      <button onClick={increment}>increment</button>
+    </div>
+  )
+}
+
+/*
 import Student from "./student";
 
 function App () {
@@ -14,7 +32,7 @@ function App () {
   )
 }
 
-/*
+
 import Welcome from "./welcome";
 
 function App() {
@@ -25,6 +43,6 @@ function App() {
   );
 }
 
-export default App;*/
+*/
 
 export default App

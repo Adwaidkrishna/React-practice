b62dc7663e57f26b97dbd2042d4e3e5c70c93388
adwaidkrishna <adwaidkrishna2@gmail.com>
2026-07-23 21:29:48 +0530

    normal counter

--- normal-counter/src/App.jsx
+++ normal-counter/src/App.jsx
@@ -1,11 +1,11 @@
 import Counter from "./counter"
 
-function App() {
+function App (){
   return (
-    <>
-      <Counter />
-    </>
+    <div>
+      <Counter/>
+    </div>
   )
 }
 
-export default App  
+export default App
--- normal-counter/src/counter.jsx
+++ normal-counter/src/counter.jsx
@@ -1,8 +1,19 @@
-function Counter() {
+import { useState } from "react"
+
+function Counter (){
+
+    const [count,setCount]=useState(0)//count current value //setcount update //use state 0 intial value
+
     return (
         <div>
-            <button>Increment</button>
+
+            <h1>counter app</h1>
             
+            <button onClick={()=>   {if (count<10)  setCount(count+1)}}>INC</button>
+            <button onClick={()=>{setCount(count-1)}}>DEC</button>
+
+            <h2>{count}</h2>
+
         </div>
     )
 }

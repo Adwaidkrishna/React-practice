83fbbf8129e884be2ab64368331d64db39076ee5
adwaidkrishna <adwaidkrishna2@gmail.com>
2026-07-24 10:01:13 +0530

    name and mobile listing

---
+++ prop-Destructure/src/student.jsx
@@ -0,0 +1,10 @@
+function Student({ name, age, stack }) {
+    return (
+        <div>
+            <h2>Name ={name}</h2>
+            <h2>Age ={age}</h2>
+            <h2>Stack ={stack}</h2></div>
+    )
+}
+
+export default Student
